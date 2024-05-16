0fa77518f9c3751ce9064a5423f924d2514c9474
Zubaer16 <ahmed.zubaer@northsouth.edu>
2024-05-16 17:46:41 +0600

    Revert "update userdetails"
    This reverts commit 47e99a1330ebcc36faecf36cce70ab7ba2b70370.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import SearchUser from './SearchUser';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import UserDetails from './UserDetails';
-import UserDetailsDownload from './UserDetailsDownload';
 
 function App() {
 
@@ -11,18 +10,19 @@ function App() {
 
   const addUserDetails=(userData)=>{
     setUserDetails(userData);
+    console.log(userData);
   }
-  
   
   return (
     <div className="App">
     <SearchUser addUserDetails={addUserDetails}/>
-    {userDetails && <UserDetailsDownload userDetails={userDetails}/> }
+    {
+      userDetails && <UserDetails userDetails={userDetails}/>
+    }
     
+
     </div>
   );
 }
 
 export default App;
-
-
--- src/UserDetails.jsx
+++ src/UserDetails.jsx
@@ -1,92 +1,34 @@
-import React from 'react';
+import React from 'react'
+import UserBasicInfo from './UserBasicInfo'
+import UserAdditionalInfo from './UserAdditionalInfo';
 
-const UserDetails = ({ userDetails }) => {
-  const bankInfo = userDetails.bank;
+const UserDetails = ({userDetails}) => {
+  const bankInfo=userDetails.bank
 
   const excludeKeys = (obj, keysToExclude) =>
     Object.fromEntries(
       Object.entries(obj).filter(([key, _]) => !keysToExclude.includes(key))
     );
 
-  const keysToExclude = ['bank', 'customers'];
+  const keysToExclude = ['accountCode', 'boId'];
 
-  const newObject = excludeKeys(userDetails, keysToExclude);
-  // console.log(newObject); // Output: { a: 1, c: 3 }
+  const userAdditionalInfo = excludeKeys(userDetails, keysToExclude);
+  
 
   // basic info
-  const userBasicInfo = {
-    accountCode: userDetails.accountCode,
-    boId: userDetails.boId
-  };
+  const userBasicInfo={
+    "accountCode":userDetails.accountCode,
+    "boId":userDetails.boId
+  }
 
-  // Customer info
-  const customerInfo = userDetails.customers && userDetails.customers[0];
+  console.log(userBasicInfo)
 
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-lg-8">
-          <div className="card mb-4">
-            <div className="card-body">
-              <h2 className="mb-4 text-center">User Basic Info</h2>
-              <div className="info-box">
-                <div className="info-item">
-                  <span className="info-label">Account Code:</span>{' '}
-                  {userBasicInfo.accountCode}
-                </div>
-                <div className="info-item">
-                  <span className="info-label">BO Id:</span>{' '}
-                  {userBasicInfo.boId}
-                </div>
-              </div>
-            </div>
-          </div>
-          {customerInfo && (
-            <div className="card">
-              <div className="card-body">
-                <h2 className="mb-4 text-center">Customer Info</h2>
-                <div className="info-box">
-                  <div className="info-item">
-                    <span className="info-label">Birth Date:</span>{' '}
-                    {customerInfo.birthDate}
-                  </div>
-                  <div className="info-item">
-                    <span className="info-label">Customer Type:</span>{' '}
-                    {customerInfo.customerType}
-                  </div>
-                  <div className="info-item">
-                    <span className="info-label">Father/Husband Name:</span>{' '}
-                    {customerInfo.fatherHusbandName}
-                  </div>
-                  <div className="info-item">
-                    <span className="info-label">Gender:</span>{' '}
-                    {customerInfo.gender}
-                  </div>
-                  <div className="info-item">
-                    <span className="info-label">Last Name:</span>{' '}
-                    {customerInfo.lastName}
-                  </div>
-                  <div className="info-item">
-                    <span className="info-label">Mother Name:</span>{' '}
-                    {customerInfo.motherName}
-                  </div>
-                  <div className="info-item">
-                    <span className="info-label">National ID:</span>{' '}
-                    {customerInfo.nationalId}
-                  </div>
-                  <div className="info-item">
-                    <span className="info-label">Nationality:</span>{' '}
-                    {customerInfo.nationality}
-                  </div>
-                </div>
-              </div>
-            </div>
-          )}
-          
-        </div>
-      </div>
+    <div>
+      <UserBasicInfo userBasicInfo={userBasicInfo}/>
+      <UserAdditionalInfo userAdditionalInfo={userAdditionalInfo} />
     </div>
-  );
-};
+  )
+}
 
 export default UserDetails;
